b0f910df2e1a7515c972c43f964f5a18373a511f
Johnny Kirpalani <johnnys-mac@Johnnys-MacBook-Pro.local>
2020-01-07 10:45:29 -0500

    Initial commit. Added Navigation.

---
+++ src/components/header/Header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import { ReactComponent as Logo } from '../../assets/logo.png';
+import './header.styles.scss';
+
+const Header = () => (
+  <div className="header">
+    <Link className="logo-container" to="/"></Link>
+    <div className="options">
+      <Link className="option" to="/#projects">
+        PROJECTS
+      </Link>
+      <Link className="option" to="/#about">
+        ABOUT
+      </Link>
+      <Link className="option" to="/#contact">
+        CONTACT
+      </Link>
+      <Link className="option" to="/#toolbox">
+        TOOLBOX
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
