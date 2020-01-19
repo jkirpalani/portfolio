dda3f2b0b1f58ae04d333d63d3d568ecda35178c
jkirpalani <jkirpalani@gmail.com>
2020-01-19 17:41:37 -0500

    Changed hero image. Updated color scheme based on new style guide.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import './homepage.styles.css';
 import CustomButton from '../components/custom-button/custom-button';
-import Background from '../assets/images/vippng.com-robot-png-5143307.png';
+import HeroImg from '../assets/images/img_hero.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Homepage = () => (
   <div className="homepage">
     <div className="left-content-hero">
-      <h2 className="hero-h2">Hello!</h2>
+      <h2 className="hero-h2">Hello.</h2>
 
       <h1 className="hero-h1">
-        My name is <span className="highlight">Johnny Kirpalani</span>.
+        My Name Is <span className="highlight">Johnny Kirpalani</span>.
       </h1>
       <h2 className="hero-h3">
         I'm a <span className="highlight">Full Stack Software Engineer </span>
-        from NYC.
+        From NYC.
       </h2>
       <div className="link-and-button-container">
         <CustomButton>
           Explore My Projects
           <span>
             {'  '}
-            <FontAwesomeIcon icon="rocket" className="nav-icon" />
+            <FontAwesomeIcon icon="rocket" className="fa-icon" />
           </span>
         </CustomButton>
         <a href src="" className="resume-link" pulse>
           View My Resume
           <span>
             {'  '}
-            <FontAwesomeIcon icon="file" className="nav-icon" />
+            <FontAwesomeIcon icon="file" className="fa-icon" />
           </span>
         </a>
       </div>
@@ -36,7 +36,7 @@ const Homepage = () => (
 
     <div
       className="background-image"
-      style={{ backgroundImage: `url(${Background})` }}
+      style={{ backgroundImage: `url(${HeroImg})` }}
     />
   </div>
 );
